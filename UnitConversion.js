a68401af2a6a87921f3f3c103c66d9f06319cc20
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-12-01 09:29:12 +0530

    Added: Unit conversion file is added

---
+++ UnitConversion.js
@@ -0,0 +1,15 @@
+// Conversion of inch to feet
+let a = 42;
+let b = 42 *(1/12);
+console.log("42 inch is equal to: "+b +" "+"feet");
+
+// Conversion feet to meter
+let d = 60;
+let e = 40;
+let c = (d*e)*0.3048;
+console.log("60 * 40 feet is equal to :"+c+" "+"meter");
+
+//Conversion of 25 such plot into acres
+let area = 731.52;
+let inAcre = area * 0.000247;
+console.log("Area of 25 plots is: "+(inAcre*25)+" "+"Acres");
